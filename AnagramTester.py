118b9522adf25e7f7f2eb4c198e55030d7d3509a
ARW <33567287+NerdyTeacher123@users.noreply.github.com>
2021-04-26 21:40:15 -0500

    Update AnagramTester.py

--- AnagramTester.py
+++ AnagramTester.py
@@ -1,3 +1,6 @@
+# Given two words, are they anagrams of each other?
+
+
 alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
@@ -5,7 +8,6 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67
 
 def wordToID(word):
   ID = []
-
   for letter in word:
     n = alphabet.index(letter)
     ID.append(primes[n])
@@ -15,7 +17,7 @@ def wordToID(word):
 def compareWords(a, b):
   x = wordToID(a)
   y = wordToID(b)
-
+  
   if set(x) == set(y):
     print("This is an Anagram")
     return True
@@ -24,6 +26,8 @@ def compareWords(a, b):
     return False
 
 
+#####################################
 compareWords("brainy", "binary")
 compareWords("restful", "fluster")
+compareWords("bats", "tabs")
 
